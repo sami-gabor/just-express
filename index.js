d13732474e48327303b1264de12ea5e93f1d68a0
Samoila Gabor <samoila.gabor@iquestgroup.com>
2019-11-20 16:21:29 +0200

    Serve static files

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const path = require('path');
+
 const express = require('express');
 
 const app = express();
 
+
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
     res.send('Express home GET');
@@ -19,8 +23,12 @@ app.delete('/', function(req, res) {
     res.send('Express home DELETE');
 });
 
+app.all('/home', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
 app.all('*', function(req, res) {
-    res.send('Express home ALL');
+    res.send('No page found...');
 });
 
 
