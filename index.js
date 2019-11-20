ad26f34109fa7f70d9f47c8916d5b1b274f89555
Samoila Gabor <samoila.gabor@iquestgroup.com>
2019-11-20 14:15:20 +0200

    Add routes

--- index.js
+++ index.js
@@ -2,8 +2,26 @@ const express = require('express');
 
 const app = express();
 
-app.get('*', function(req, res) {
-    res.send('Express !')
-})
+
+app.get('/', function(req, res) {
+    res.send('Express home GET');
+});
+
+app.post('/', function(req, res) {
+    res.send('Express home POST');
+});
+
+app.put('/', function(req, res) {
+    res.send('Express home PUT');
+});
+
+app.delete('/', function(req, res) {
+    res.send('Express home DELETE');
+});
+
+app.all('*', function(req, res) {
+    res.send('Express home ALL');
+});
+
 
 app.listen(3000);
