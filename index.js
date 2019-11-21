2979e57c6cc33822727e059c04dcbd83b1a65ad0
Samoila Gabor <samoila.gabor@iquestgroup.com>
2019-11-21 13:20:02 +0200

    Mimic a POST request

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const path = require('path');
-
 const express = require('express');
 
 const app = express();
@@ -12,31 +11,41 @@ const myMiddleware = (req, res, next) => {
 }
 
 app.use(express.static('public'));
-
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 app.use('/mid', myMiddleware);
+
 
 app.get('/', function(req, res) {
     res.send('Express home GET');
 });
 
+
 app.post('/', function(req, res) {
     res.send('Express home POST');
 });
+
+app.post('/ajax', function(req, res) {
+    console.log('Response from POST/ajax: ', req.body.name);
+    res.send('POST/ajax');
+});
+
 
 app.put('/', function(req, res) {
     res.send('Express home PUT');
 });
 
+
 app.delete('/', function(req, res) {
     res.send('Express home DELETE');
 });
+
 
 app.all('/home', function(req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.all('*', function(req, res) {
-    console.log(res.locals.myMiddleStuff);
     res.send('No page found...');
 });
 
