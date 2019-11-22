330b83f863387cc4cdf624fcce2467d287003501
Samoila Gabor <samoila.gabor@iquestgroup.com>
2019-11-22 13:31:37 +0200

    Create logout page

--- login-site/server.js
+++ login-site/server.js
@@ -3,12 +3,15 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+const cookieParser = require('cookie-parser');
+
 const helmet = require('helmet');
 app.use(helmet());
 
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +26,12 @@ app.get('/login', function(req, res) {
 });
 
 app.get('/welcome', function(req, res) {
-    res.render('welcome');
+    res.render('welcome-page', { email: req.cookies.email });
+});
+
+app.get('/process_logout', function(req, res) {
+    res.clearCookie('email');
+    res.redirect('/login');
 });
 
 
@@ -32,7 +40,7 @@ app.post('/process_login', function(req, res) {
     const password = req.body.password;
 
     if (password === 'x') {
-        res.cookie('password', password);
+        res.cookie('email', email);
         res.redirect('/welcome');
     } else {
         res.redirect('/login');
