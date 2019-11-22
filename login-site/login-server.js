4dff735631004dc9eeb7889a5cb1416a0ee94e63
Samoila Gabor <samoila.gabor@iquestgroup.com>
2019-11-22 13:13:20 +0200

    Create login and welcome pages

---
+++ login-site/login-server.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+const express = require('express');
+const app = express();
+
+const helmet = require('helmet');
+app.use(helmet());
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.get('/', function(req, res) {
+    res.send('Express started!');
+});
+
+app.get('/login', function(req, res) {
+    res.render('login-page');
+});
+
+app.get('/welcome', function(req, res) {
+    res.render('welcome');
+});
+
+
+app.post('/process_login', function(req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    if (password === 'x') {
+        res.cookie('password', password);
+        res.redirect('/welcome');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+
+app.listen(3000, console.log(new Date().toLocaleTimeString() + ' - listening on port 3000'));
