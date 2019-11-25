2bb97606b590c5a24d1190c5644f12a2a7d661bc
Samoila Gabor <samoila.gabor@iquestgroup.com>
2019-11-25 10:51:46 +0200

    Initial Router

---
+++ router-app/routerApp.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const helmet = require('helmet');
+app.use(helmet());
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.static('public'));
+
+
+const router = require('./theRouter');
+app.use('/', router);
+
+
+app.listen(3000);
