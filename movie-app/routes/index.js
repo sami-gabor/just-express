7396c714e74ddd4db2ad99d20ffaa114c5055273
Samoila Gabor <samoila.gabor@iquestgroup.com>
2019-11-26 11:58:50 +0200

    Load movie images

--- movie-app/routes/index.js
+++ movie-app/routes/index.js
@@ -5,14 +5,18 @@ const request = require('request');
 const apiKey = '1fb720b97cc13e580c2c35e1138f90f8';
 const apiBaseUrl = 'http://api.themoviedb.org/3';
 const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
-const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
+const imageBaseUrl = 'http://image.tmdb.org/t/p/w200';
+
+router.use('/', (req, res, next) => {
+  res.locals.imageBaseUrl = imageBaseUrl;
+  next();
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   request.get(nowPlayingUrl, (error, response, movieData) => {
     const parsedData = JSON.parse(movieData);
-    // res.json(parsedData);
-    res.render('index', { parsedData: parsedData.results });
+    res.render('index', { movies: parsedData.results });
   });
 });
 
