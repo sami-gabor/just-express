0a11659d2617039841b0fb3329840187792f1309
Samoila Gabor <samoila.gabor@iquestgroup.com>
2019-11-26 09:43:45 +0200

    Add request module, request data from api

--- movie-app/routes/index.js
+++ movie-app/routes/index.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const request = require('request');
+
+const apiKey = '1fb720b97cc13e580c2c35e1138f90f8';
+const apiBaseUrl = 'http://api.themoviedb.org/3';
+const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
+const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  request.get(nowPlayingUrl, (error, response, movieData) => {
+    const parsedData = JSON.parse(movieData);
+    console.log(parsedData);
+    
+  });
   res.render('index', { title: 'Movie App' });
 });
 
